26257f09afdc1462975072fb3b5114f8908630ef
Alan de Oliveira Gonçalves <alandeog@poli.ufrj.br>
2024-02-25 18:23:40 -0300

    Update app component and home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [MenuTitleComponent],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
