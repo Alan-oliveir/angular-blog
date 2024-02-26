6a9eb2f534761bc5e70cf065c2ac92194a637516
Alan de Oliveira Gonçalves <alandeog@poli.ufrj.br>
2024-02-25 22:56:00 -0300

    Update header and menu title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [RouterOutlet, HomeComponent],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-blog';
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
-import { BigCardComponent } from '../../components/big-card/big-card.component';
-import { SmallCardComponent } from '../../components/small-card/small-card.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
-  imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent],
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
